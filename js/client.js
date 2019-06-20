8ffb7feba6c716ce50bce79269449b8b077ea34a
Sean Olson <srolson@callutheran.edu>
2019-06-20 11:52:13 -0500

    added click, functions to render students/studentList

--- js/client.js
+++ js/client.js
@@ -13,6 +13,30 @@ function onReady () {
     console.log('jQuery works');
     // call the function 'renderStudentList' to display to DOM via jQuery
     renderStudentList(playfair);
+
+    // create a 'click' event, using function showAwesomeAlert
+    // Need to select something already on the DOM 
+    // Can filter the event to something new (dynamically generated)
+
+    // shouldn't do this! Because it will break later! 
+    $('.student').on('click', showAwesomeAlert);
+
+    // // -- do this with the 2nd arg to 'on' method ('.student')
+    // $('#student').on('click', '.student', showAwesomeAlert);
+    console.log(event);
+    console.log('This:', this);
+}
+
+// This function will be called when we click on a student 
+// It will show an alert saying the student is awesome 
+
+function showAwesomeAlert (event) {
+    console.log('Event', event);
+    // In the event handler (**EVENT HANDLERS ARE ALWAYS FUNCTIONS**)
+    // 'this' is what generated the event 
+    // -- 'this' is always contextually 
+    console.log(this);
+    alert('Check the console!');
 }
 
 // add function to add students to DOM that sends in an array of students 
@@ -32,5 +56,5 @@ function renderStudentList (studentList) {
 // ID from HTML, and appends each student object, one at a time,
 // into the DOM in an unordered list 
 function renderStudent(student) {
-    $('#students').append(`<li>${student}</li>`);
+    $('#students').append(`<li class="student">${student}</li>`);
 }
